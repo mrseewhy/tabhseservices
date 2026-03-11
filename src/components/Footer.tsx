f14dde5cdd6693197b2af8d5ee51fbfcf6712136
Olusegun Adeyemi <53488450+mrseewhy@users.noreply.github.com>
2026-03-11 01:02:54 +0100

    footer fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import LinkButton from "./LinkButton"
 
 const Footer = () => {
     return (
-        <div className="relative overflow-hidden bg-linear-to-br from-black via-[#0b0f1f] to-[#0f172a] text-whitemt-24 pb-6 py-16">
+        <div className="relative overflow-hidden bg-linear-to-br from-black via-[#0b0f1f] to-[#0f172a] text-white  pb-6 py-16">
 
             {/* ── Dot grid overlay ── */}
             <div
@@ -67,7 +67,7 @@ const Footer = () => {
                         <h5 className="text-lg font-semibold uppercase tracking-wide text-(--my-purple) md:text-xl">
                             Contact
                         </h5>
-                        <div className="flex flex-col">
+                        <div className="flex flex-col text-gray-400">
                             {/* Phone */}
                             <div className="flex gap-3 items-center hover:bg-[#19151520] rounded-lg p-2 transition-colors">
                                 <div className="h-8 w-8 flex items-center justify-center bg-[#ffbbff] rounded-lg text-(--my-purple)">
@@ -77,7 +77,7 @@ const Footer = () => {
                                     </svg>
                                 </div>
                                 <div className="flex flex-col min-w-0">
-                                    <h4 className="text-sm font-bold">Phone</h4>
+                                    <h4 className="text-sm font-bold tracking-wider">Phone</h4>
                                     <p className="text-xs truncate"><a href="tel:+2348127107336" target="_blank">+234 812 710 7336</a></p>
                                 </div>
                             </div>
@@ -90,7 +90,7 @@ const Footer = () => {
                                     </svg>
                                 </div>
                                 <div className="flex flex-col min-w-0">
-                                    <h4 className="text-sm font-bold">Email</h4>
+                                    <h4 className="text-sm font-bold tracking-wider">Email</h4>
                                     <p className="text-xs truncate"><a href="mailto:info@tabhseservices.com" target="_blank">info@tabhseservices.com</a></p>
                                 </div>
                             </div>
@@ -103,7 +103,7 @@ const Footer = () => {
                                     </svg>
                                 </div>
                                 <div className="flex flex-col min-w-0">
-                                    <h4 className="text-sm font-bold">Office</h4>
+                                    <h4 className="text-sm font-bold tracking-wider">Office</h4>
                                     <p className="text-xs"><a href="https://maps.google.com/?q=Enahoro+Housing+Estate+Ogba+Lagos" target="_blank">Enahoro Housing Estate, Ogba, Lagos State.</a></p>
                                 </div>
                             </div>
@@ -116,7 +116,7 @@ const Footer = () => {
                                     </svg>
                                 </div>
                                 <div className="flex flex-col min-w-0">
-                                    <h4 className="text-sm font-bold">LinkedIn</h4>
+                                    <h4 className="text-sm font-bold tracking-wider">LinkedIn</h4>
                                     <p className="text-xs truncate"><a href="https://www.linkedin.com/company/tab-hse-services" target="_blank">TAB HSE Services</a></p>
                                 </div>
                             </div>
